4aa234f1c98ea9f1a423b2a0b03d06916ff6cc01
harnoor1511 <harnoorarora15@gmail.com>
2025-11-01 23:25:40 +0530

    basic extract script

---
+++ extractframes.py
@@ -0,0 +1,14 @@
+import cv2
+
+v = cv2.VideoCapture("video.mp4")
+n = 0
+
+while True:
+    r, f = v.read()
+    if not r:
+        break
+    cv2.imwrite("frame" + str(n) + ".jpg", f)
+    n = n + 1
+
+v.release()
+print("done")
